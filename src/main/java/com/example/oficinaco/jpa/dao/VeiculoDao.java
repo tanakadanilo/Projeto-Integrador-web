ddbf6de5e6e497308c69397ce178ed9e7d8a5cd3
Danilo Tanaka <tanakadanilo867@gmail.com>
2022-12-01 10:34:41 -0300

    criando tela de modelo
    auto complete não funciona corretamente

--- src/main/java/com/example/oficinaco/jpa/dao/VeiculoDao.java
+++ src/main/java/com/example/oficinaco/jpa/dao/VeiculoDao.java
@@ -1,16 +1,24 @@
 package com.example.oficinaco.jpa.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import com.example.oficinaco.jpa.entidade.Modelo;
 import com.example.oficinaco.jpa.entidade.Veiculo;
 
 @Repository
-public interface VeiculoDao extends JpaRepository<Veiculo, Integer>{
-	
+public interface VeiculoDao extends JpaRepository<Veiculo, Integer> {
+
 	@Query(nativeQuery = true, value = "select * from veiculo where lower(placa) = lower(:placa)")
 	Veiculo consultarPorPlaca(@Param("placa") String placa);
 
+	@Query(nativeQuery = true, value = "select * from modelo m where lower(m.nome) like  lower(:nome)")
+	List<Modelo> completeModelo(@Param("nome") String nome);
+
+	@Query(nativeQuery = true, value = "select * from veiculo v join modelo m on modelo_id  = m.id where lower(m.nome) like lower(:nome)")
+	List<Veiculo> completeVeiculo();
 }
--- src/main/java/com/example/oficinaco/jpa/jsf/VeiculoControl.java
+++ src/main/java/com/example/oficinaco/jpa/jsf/VeiculoControl.java
@@ -1,6 +1,7 @@
 package com.example.oficinaco.jpa.jsf;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
@@ -8,8 +9,10 @@ import javax.faces.bean.SessionScoped;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.ui.Model;
 
 import com.example.oficinaco.jpa.dao.VeiculoDao;
+import com.example.oficinaco.jpa.entidade.Modelo;
 import com.example.oficinaco.jpa.entidade.Veiculo;
 
 @Component
@@ -63,6 +66,15 @@ public class VeiculoControl {
 
 	public void setVeiculos(List<Veiculo> veiculos) {
 		this.veiculos = veiculos;
+	}
+
+	public List<Modelo> completeModelo(String nome) {
+		List<Modelo> modelos = new ArrayList<>();
+		for (Modelo veiculo : veiculoDao.completeModelo(nome)) {
+			modelos.add(veiculo);
+		}
+
+		return modelos;
 	}
 
 }
